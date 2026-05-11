3ea96f9f1cb6877dac66bd7074564d1e1a1f4844
bisnisyudi68-lang <bisnisyudi68@gmail.com>
2026-05-11 22:49:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const symbols = [
 "🎁"
 
 ];
+
 let coin = 1000;
 
 function randomSymbol(){
@@ -24,37 +25,25 @@ function spin(){
     .innerText = "Coin habis!";
 
     return;
+
   }
 
   coin -= 50;
 
-  let a = randomSymbol();
-  let b = randomSymbol();
-  let c = randomSymbol();
+  let reels =
+  document.querySelectorAll(".reel");
 
-  document.getElementById("r1").innerText = a;
-  document.getElementById("r2").innerText = b;
-  document.getElementById("r3").innerText = c;
+  reels.forEach(function(reel){
 
-  let result = "Kalah 😢";
+    reel.innerText =
+    randomSymbol();
 
-  if(a === b && b === c){
-
-    coin += 300;
-    result = "JACKPOT 🎉 +300";
-
-  }
-  else if(a === b || b === c || a === c){
-
-    coin += 100;
-    result = "Menang kecil ✨ +100";
-
-  }
+  });
 
   document.getElementById("coin")
   .innerText = coin;
 
   document.querySelector(".result")
-  .innerText = result;
+  .innerText = "SPIN BERHASIL 🎰";
 
 }
